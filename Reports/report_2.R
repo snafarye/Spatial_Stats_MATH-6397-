c0caf240d9c15b58b91141b8c4355df2d41d60cb
snafarye <84543239+snafarye@users.noreply.github.com>
2021-10-28 13:55:02 -0500

    Add files via upload

---
+++ Reports/report_2.R
@@ -0,0 +1,47 @@
+library(readxl)
+load('C:/Users/nsara/Documents/Grad_School/MATH 6397/report/data1.RData')
+dim(data1)
+
+#---------------------- Data Cleaning ------------------------------------------
+
+general_data = data1 #Create a dataframe with the loaded dataset
+nrow(general_data) #Check the number of rows of the dataset
+sum(is.na(general_data)) # have about 20 rows with at least one missing value
+colSums(is.na(general_data))  # check to see what columns have the missing values 
+
+new_data <- na.omit(general_data) #create a new dataset without missing values
+nrow(new_data) #check number of rows of new dataset without missing values
+sum(is.na(new_data)) #check to see if all missing values have been removed
+
+set.seed(111)
+sample_data = new_data[sample(1:dim(new_data)[1],1500), ] #create a sample dataset of 1500 rows
+nrow(sample_data)#check to see if sample dataset has 1500 rows
+
+sample_kriging_data = new_data[sample(setdiff(1:dim(new_data)[1], sample_data),100),] # 100 location for kriging
+nrow(sample_kriging_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
