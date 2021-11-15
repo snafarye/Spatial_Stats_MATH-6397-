0b1e12eed4d731ebc0230d89a62cfc0afcf3345b
snafarye <84543239+snafarye@users.noreply.github.com>
2021-11-15 17:05:57 -0600

    Update Lec_17.R

--- Lec_Notes/Lec_17.R
+++ Lec_Notes/Lec_17.R
@@ -1,31 +1,23 @@
-
-
 ##install.packages("spacetime")
-
-
 ##library(spacetime)
 
 library(gstat)
-
 library(sp)
-
 library(fields)
 
 
-data("wind")
-
+data("wind") # in the gstat package 
+head(wind)
+# change long, lat to numerical values 
 wind.loc$y <- as.numeric(char2dms(as.character(wind.loc[["Latitude"]])))
-
 wind.loc$x <- as.numeric(char2dms(as.character(wind.loc[["Longitude"]])))
 
 plot(wind.loc$x, wind.loc$y,xlim=c(-10.5,-5), ylim=c(51, 56),pch=20, col=2)
-
 world(add=T)
-
 text(wind.loc$x, wind.loc$y, label=wind.loc$Code,col="purple")
 
+#combine time into 1 column
 wind$time <- ISOdate(wind$year + 1900, wind$month, wind$day)
-
 wind$jday <- as.numeric(format(wind$time, "%j"))
 
 wind1=wind[1:365, c(4:15)]
@@ -33,35 +25,38 @@ wind1=t(wind1)
 loc=cbind(wind.loc$x, wind.loc$y) ## longitude and latitude
 
 plot(1:365, seq(0, max(wind1), ,365), type="n")
-
+# time series , over time, diff color = diff stations 
 for(i in 1:12){
   lines(wind1[i,],col=i)
 }
-
+# is there a pattern b/c long, lat, does the wind speed change with long, lat, each box for each loc, y = speed
+# not much treand, or change in vaeriance 
 boxplot(wind1~loc[,1])
 
+#interquartile range, the box, does not seem to change much over time 
 boxplot(wind1~loc[,2])
 
-sp.mean=colSums(wind1)/12
-time.mean=rowSums(wind1)/365
+#spatial and temporal mean
+sp.mean=colSums(wind1)/12  #avg space over 12 stations 
+time.mean=rowSums(wind1)/365 # avg of time at each 12 station
 
-plot(1:365, sp.mean,type="l")
+# the average over the 12 locations 
+#no temporal trend or inc/dec pattern
+
+plot(1:365, sp.mean,type="l")  #time plot
 
 library(viridis)
 
-cols=inferno(100)[100:1]
-
+cols=inferno(100)[100:1]       # space plot
 quilt.plot(loc, time.mean,xlim=c(-10.5,-5), ylim=c(51, 56),col=cols)
 world(add=T)
 
+
 z=matrix(wind1, ncol=1, byrow=FALSE)
-
-
 hist(z)
-qqnorm(z)
-
+qqnorm(z);qqline(z, col = "red")
 hist(log(z))
-qqnorm(log(z))
+qqnorm(log(z)); qqline(log(z), col = "red")
 
 
 
@@ -69,6 +64,8 @@ qqnorm(log(z))
 
 
 
+#
+# new space time locations , long, lat repeated 365 times 
 
 time=matrix(sort(rep(1:365, 12)),ncol=1)
 
@@ -76,7 +73,9 @@ loc1=NULL
 for(i in 1:365){
   loc1=rbind(loc1, loc)
 }
+dim(loc1) # 4380 = 12*365
 
+# spatrial loc distances 
 D.s=rdist.earth(loc1, miles=FALSE) ## spatial distance (large)
 D.t=rdist(time) ## temporal distance (large)
 
@@ -85,28 +84,29 @@ D.t1=rdist(1:365)## temporal distance (small)
 
 z1=z-mean(z)
 
+
+# calc log-likelihood under seperable 
 loglik.sep=function(par){
   
   print(par)
-  
+  # take the exp to have positive values in the end
   alpha=exp(par[1])
   beta1=exp(par[2])
   beta2=exp(par[3])
   
   S1=exp(-D.s1/beta1)
   S2=exp(-D.t1/beta2)
+  S=alpha*kronecker(S1, S2) # s, t under seperable 
   
-  S=alpha*kronecker(S1, S2)
+  part1=determinant(S, logarithm=T)$modulus  # give the log of the determinate 
+  part2=t(z1) %*% solve(S) %*% z1
   
-  ##part1=determinant(S, logarithm=T)$modulus
-  ##part2=t(z1) %*% solve(S) %*% z1
- 
-  K=chol(S)
-  part1=sum(log(diag(K)))*2
+  #K=chol(S)
+  #part1=sum(log(diag(K)))*2
   
-  temp=forwardsolve(t(K),z1 )
-  temp=backsolve(K, temp)
-  part2=t(z1) %*% temp
+  #temp=forwardsolve(t(K),z1 )
+  #temp=backsolve(K, temp)
+  #part2=t(z1) %*% temp
   
   temp=part1+part2
   
@@ -117,7 +117,7 @@ loglik.sep=function(par){
 }
 
 
-
+# 1st approch 
 loglik.sym=function(par){
   
   print(par)
@@ -127,20 +127,16 @@ loglik.sym=function(par){
   beta2=exp(par[3])
   
   D=sqrt((D.s/beta1)^2+(D.t/beta2)^2)
+    S=alpha*exp(-D)
   
-  S=alpha*exp(-D)
-  
-  K=chol(S)
-  part1=sum(log(diag(K)))*2
-  
-  temp=forwardsolve(t(K),z1 )
-  temp=backsolve(K, temp)
-  part2=t(z1) %*% temp
- 
+  #K=chol(S)
+  #part1=sum(log(diag(K)))*2
+    #temp=forwardsolve(t(K),z1 )
+  #temp=backsolve(K, temp)
+  #part2=t(z1) %*% temp
   
   temp=part1+part2
-  
-  print(-temp)
+    print(-temp)
   return(temp)
   
 }
@@ -240,4 +236,3 @@ d=sqrt((d.s/beta1)^2+ (d.t/beta2)^2)
 k=alpha*exp(-d)
 
 lambda=solve(S) %*%k
-
